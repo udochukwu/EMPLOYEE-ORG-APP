89c217a433b4f519cc1dc263dceebfdf2cac09db
nelson <nnaji_udochukwu@yahoo.com>
2023-01-23 22:21:23 +0100

    custom dropdown

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Dropdown from './Dropdown';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Dropdown /> */}
   </React.StrictMode>
 );
 
